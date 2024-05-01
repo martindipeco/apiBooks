5fd9081684f33896a6048687ec1f94bc6875f5b1
martindipeco <martindipeco@gmail.com>
2024-05-01 15:20:09 -0300

    primer prueba conexion a API funciona busqueda

--- src/main/java/com/aluracursos/apiBooks/principal/Principal.java
+++ src/main/java/com/aluracursos/apiBooks/principal/Principal.java
@@ -8,8 +8,8 @@ import com.aluracursos.apiBooks.service.ConvierteDatos;
 import java.util.Scanner;
 
 public class Principal {
-    //se pueden buscar APIS acá : https://publicapis.dev/category/books
-    private final String URL_BASE = "https://www.omdbapi.com/?t="; //https://gutendex.com/
+    //se pueden buscar más APIS acá : https://publicapis.dev/category/books
+    private final String URL_BASE = "https://gutendex.com/books/?search=";
     //private final String API_KEY = ApiKey.getApiKey();
     private Scanner scanner = new Scanner(System.in);
     private ConsumoApi consumoAPI = new ConsumoApi();
@@ -24,8 +24,20 @@ public class Principal {
         System.out.println("Ingrese su búsqueda");
         var busqueda = scanner.nextLine();
         //String direccion = URL_BASE + busqueda.replace(" ", "+") + "&apikey=" + API_KEY;
-        String direccion = URL_BASE + busqueda.replace(" ", "+");
+        String direccion = URL_BASE + busqueda.replace(" ", "%20");
         var json = consumoAPI.obtenerDatos(direccion);
+        System.out.println(json);
         var datos = conversor.obtenerDatos(json, DatosLibro.class);
+
+        //Como lidiar con resultado que es un array
+        //String jsonString = peli.buscaPeliculaPorTitulo(busquedaTitulo);
+
+        //to access values from the JSON -> parse String to Json
+        //JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
+
+        //JsonArray resultsArray = jsonObject.getAsJsonArray("results");
+        //int numeroDeResultados = jsonObject.get("count").getAsInt();
+
+
     }
 }
