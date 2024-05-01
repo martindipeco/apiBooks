a657ecf84eb0a512a365e731a7ac0ddd92437ca3
martindipeco <martindipeco@gmail.com>
2024-05-01 10:55:31 -0300

    first commit

---
+++ src/test/java/com/aluracursos/apiBooks/ApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.apiBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
